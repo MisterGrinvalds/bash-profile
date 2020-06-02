c7746d5211b5ef713faf916802f63e45609b9ebb
Ross Grinvalds <ross.grinvalds@gmail.com>
2020-06-02 10:29:21 -0400

    Add machine-specific logic
    One .bash_profile_environment to rule the all.

--- .bash_profile_environment.sh
+++ .bash_profile_environment.sh
@@ -1,14 +1,21 @@
-# Symbolic Links
-if [ ! -L "/Library/Frameworks/R.framework/Resources/etc/Rprofile.site" ]; then
-    ln -s "$DOTFILES/.R/Rprofile.site" "/Library/Frameworks/R.framework/Resources/etc/"
+# Machine-Specific
+export MACHINE_NAME="Ross-MBP"
+if [ "$HOSTNAME" = "$MACHINE_NAME" ]; then
+    # RStudio Hack to force it to use .dotfiles configuration
+    if [ ! -L "/Library/Frameworks/R.framework/Resources/etc/Rprofile.site" ]; then
+        ln -s "$DOTFILES/.R/Rprofile.site" "/Library/Frameworks/R.framework/Resources/etc/"
+    fi
+
+    if [ ! -d /Applications/FileZilla.app ]; then
+        cp "$DOTFILES/.docs/fzdefaults.xml" /Applications/FileZilla.app/Contents/SharedSupport
+    fi
+
+    export XDG_DATA_HOME="$HOME/Library/Application Support"
+    export XDG_CACHE_HOME="$HOME/Library/Caches"
+    echo test
 fi
 
-# Manual config writing:
-if [ ! -d /Applications/FileZilla.app ]; then
-    cp "$DOTFILES/.docs/fzdefaults.xml" /Applications/FileZilla.app/Contents/SharedSupport
-fi
-
-# Shell Environment Variables
+# All Machines
 export GIT_CONFIG="$DOTFILES/.gitconfig"
 export IPYTHONDIR="$DOTFILES/.ipython"
 export JUPYTER_CONFIG_DIR="$DOTFILES/.jupyter"
@@ -17,5 +24,4 @@ export NVIM_LOG_FILE="$HOME/Library/Application Support"
 export R_PROFILE="$DOTFILES/.R/Rprofile.site"
 export R_PROFILE_USER="$DOTFILES/.R/.Rprofile"
 export XDG_CONFIG_HOME="$DOTFILES"
-export XDG_DATA_HOME="$HOME/Library/Application Support"
-export XDG_CACHE_HOME="$HOME/Library/Caches"
+
