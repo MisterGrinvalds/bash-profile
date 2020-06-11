9fcc33e8f468d5324cd697d292c3a81837a1b486
Ross Grinvalds <ross.grinvalds@gmail.com>
2020-06-10 23:32:22 -0700

    Reorganize
    Do this to accurately map an XDG dependency. Also, the order of the
    alphabet is hard.

--- .bash_profile/environment.sh
+++ .bash_profile/environment.sh
@@ -7,7 +7,6 @@ export NPM_CONFIG_USERCONFIG="$DOTFILES/.npmrc"
 export PYTHONSTARTUP="$DOTFILES/.python/startup.py"
 export R_PROFILE="$DOTFILES/.R/Rprofile.site"
 export R_PROFILE_USER="$DOTFILES/.R/.Rprofile"
-export VIM_PLUGGED="$DOTFILES/nvim/plugged"
 export XDG_CONFIG_HOME="$DOTFILES"
 
 # Machine Dependent
@@ -25,10 +24,11 @@ if [ "$HOSTNAME" = "$MACHINE_NAME" ]; then
     export NPM_CONFIG_GLOBALCONFIG="$DOTFILES/.npm/npmrc"
     export NVIM_LOG_FILE="$HOME/Library/Application Support"
     export SHELL_SESSION_HISTORY=0
-    export XDG_DATA_HOME="$HOME/Library/Application Support"
     export XDG_CACHE_HOME="$HOME/Library/Caches"
+    export XDG_DATA_HOME="$HOME/Library/Application Support"
 fi
 
 # XDG_CONFIG Dependent
 export HISTFILE="$XDG_CACHE_HOME"
+export VIM_PLUGGED="$XDG_CONFIG_HOME/nvim/plugged"
 
