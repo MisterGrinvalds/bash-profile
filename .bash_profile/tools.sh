d865b53b2ec5ea7a385f12dfdd49aa3152ec628a
Ross Grinvalds <ross.grinvalds@gmail.com>
2020-06-11 08:15:44 -0700

    Reorganize and Make Pithy

--- .bash_profile/tools.sh
+++ .bash_profile/tools.sh
@@ -1,10 +1,36 @@
-cd() { builtin cd "$@" && ll; }                                             # Always `ll` after a `cd`
-mktar() { tar cvzf "${1%%/}.tar.gz"  "${1%%/}/"; }                          # Creates a *.tar.gz archive of a file or folder
-mkzip() { zip -r "${1%%/}.zip" "$1" ; }                                     # Create a *.zip archive of a file or folder
-bash-as() { sudo -u $1 /bin/bash; }                                         # Run a bash shell as another user
-dirdiff() { diff -u <( ls "$1" | sort)  <( ls "$2" | sort ); }              # Compare the contents of 2 directories
-h() { history | grep "$1"; }                                                # Shorthand for `history` with added grepping
-
-if [ -f /usr/local/etc/bash_completion ]; then                              # Bring in bash-completion
+# Load Tools
+if [ -f /usr/local/etc/bash_completion ]; then
    . /usr/local/etc/bash_completion
 fi
+
+# Custom Functions
+## Always `ll` after a `cd`
+cd()
+{ 
+	builtin cd "$@" && ll 
+} 
+
+## Run a bash shell as another user
+bash-as() 
+{ 
+	sudo -u $1 /bin/bash
+} 
+
+## Shorthand for `history` with added grepping
+h() 
+{ 
+	history | grep "$1" 
+}            
+
+## Creates a *.tar.gz archive of a file or folder
+mktar() 
+{ 
+	tar cvzf "${1%%/}.tar.gz"  "${1%%/}/"
+} 
+
+## Create a *.zip archive of a file or folder
+mkzip() 
+{ 
+	zip -r "${1%%/}.zip" "$1" 
+}                                     
+
