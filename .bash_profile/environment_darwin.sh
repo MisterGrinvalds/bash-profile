7325a8ded55d29662ee287425c33487b74599432
Ross Grinvalds <ross.grinvalds@gmail.com>
2020-07-09 22:13:58 +0000

    Add Some Catches for Bash Command Logs

--- .bash_profile/environment_darwin.sh
+++ .bash_profile/environment_darwin.sh
@@ -10,5 +10,7 @@ export HISTCONTROL=$HISTCONTROL:erasedups:ignoredups:ignorespace
 export HISTFILE="$HOME/.bash_history"
 export HISTFILESIZE=3000               
 export HISTSIZE=3000                   
+export LESSHISTFILE="$DOTFILES/.lesshst"
+export WGETRC="$DOTFILES/.wgetrc"
 export XDG_CACHE_HOME="$HOME/Library/Caches"
 export XDG_DATA_HOME="$HOME/Library/Application Support"
