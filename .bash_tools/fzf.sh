8d149ea857f57f14cb0aeeccfff3016fb9f21a9e
Ross Grinvalds <ross.grinvalds@gmail.com>
2020-08-31 16:28:19 -0400

    Update FZF References

--- .bash_tools/fzf.sh
+++ .bash_tools/fzf.sh
@@ -2,3 +2,14 @@ export FZF_DEFAULT_OPS="--extended"
 export FZF_DEFAULT_COMMAND="fd --type f"
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 
+# Setup fzf
+if [[ ! "$PATH" == */usr/local/opt/fzf/bin* ]]; then
+  export PATH="${PATH:+${PATH}:}/usr/local/opt/fzf/bin"
+fi
+
+# Auto-completion
+[[ $- == *i* ]] && source "/usr/local/opt/fzf/shell/completion.bash" 2> /dev/null
+
+# Key bindings
+source "/usr/local/opt/fzf/shell/key-bindings.bash"
+
