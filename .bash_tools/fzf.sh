22bdbd0a21165ddea254ce5d9b19d4be85da9e68
Ross Grinvalds <ross.grinvalds@lilly.com>
2021-02-24 13:24:21 -0500

    Fix FZF Shell Function Location

--- .bash_tools/fzf.sh
+++ .bash_tools/fzf.sh
@@ -1,34 +1,21 @@
-export FZF_DEFAULT_OPS="--extended"
+# Environment
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
+export FZF_DEFAULT_OPS="--extended"
+
+if [[ ! "$PATH" == *$FZF_LOCATION/bin* ]]; then
+	export PATH="${PATH:+${PATH}:}"$FZF_LOCATION/bin
+fi
+
+# Auto-completion
+[[ $- == *i* ]] && source "$FZF_LOCATION/shell/completion.bash" 2> /dev/null
+
+# Key bindings
+source "$FZF_LOCATION/shell/key-bindings.bash"
 
 # OS Dependent
 if [[ "$OSTYPE" = "darwin"* ]]; then
 	export FZF_DEFAULT_COMMAND="fd --type f"
-	
-	# Setup fzf
-	if [[ ! "$PATH" == */usr/local/opt/fzf/bin* ]]; then
-		export PATH="${PATH:+${PATH}:}/usr/local/opt/fzf/bin"
-	fi
-	
-	# Auto-completion
-	[[ $- == *i* ]] && source "/usr/local/opt/fzf/shell/completion.bash" 2> /dev/null
-
-	# Key bindings
-	source "/usr/local/opt/fzf/shell/key-bindings.bash"
-
 elif [[ "$OSTYPE" = "linux-gnu" ]]; then
 	export FZF_DEFAULT_COMMAND="fdfind --type f"
-	
-	# Setup fzf
-	if [[ ! "$PATH" == */usr/bin* ]]; then
-		export PATH="${PATH:+${PATH}:}/usr/bin"
-	fi
-
-	# Auto-completion
-	[[ $- == *i* ]] && source "/usr/share/doc/fzf/examples/completion.bash" 2> /dev/null
-
-	# Key bindings
-	source "/usr/share/doc/fzf/examples/key-bindings.bash"
-
 fi
 
