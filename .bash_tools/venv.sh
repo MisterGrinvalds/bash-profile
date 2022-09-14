c83d5b6cfa792dde8fbf0003b5ec27bdc8e362ca
MisterGrinvalds <ross.grinvalds@gmail.com>
2022-09-14 16:25:00 -0400

    update venv command for mkvenv changes

--- .bash_tools/venv.sh
+++ .bash_tools/venv.sh
@@ -1,1 +1,8 @@
-function venv() { export VENV=$1 && source $ENVS_LOCATION/$VENV/bin/activate ; }
+function venv() { 
+	if [ -z "$1" ]
+	then
+			source .venv/bin/activate
+	else
+		export VENV=$1 && source $ENVS_LOCATION/$VENV/bin/activate ; 
+	fi
+}
