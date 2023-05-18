52cb888725fcfaf84ee7e8cd3d914e4fb31c1c16
MisterGrinvalds <ross.grinvalds@gmail.com>
2023-05-18 13:25:09 -0400

    add args for colima

--- .bash_tools/colima-no-proxy.sh
+++ .bash_tools/colima-no-proxy.sh
@@ -1,8 +1,26 @@
 ## Setup container run time for colima without proxies
 colima-no-proxy()
 {
+	VCPUS=4
+	GB_RAM=8
+	STORAGE=20
+  	COLIMA_SOCK="$COLIMA_SOCK"
+	COLIMA_HOST="$COLIMA_HOST"
+
+	while getopts 'u:t:x' flag
+	do
+		case "${flag}" in
+			c) VCPUS="${OPTARG}";;
+			m) GB_RAM="${OPTARG}";;
+			d) STORAGE="${OPTARG}";;
+			s) COLIMA_SOCK="${OPTARG}";; 
+			h) COLIMA_HOST="${OPTARG}";; 
+		esac
+	done
+
 	colima stop
+	set-proxy
 	rmenv
 	set-docker-host $COLIMA_HOST $COLIMA_SOCK
-	colima start
+	colima start --cpu $VCPUS --memory $GB_RAM --disk $STORAGE
 }
--- .bash_tools/colima-proxy.sh
+++ .bash_tools/colima-proxy.sh
@@ -1,8 +1,25 @@
 ## Setup container run time for colima with proxies
 colima-proxy()
 {
+	VCPUS=4
+	GB_RAM=8
+	STORAGE=20
+  	COLIMA_SOCK="$COLIMA_SOCK"
+	COLIMA_HOST="$COLIMA_HOST"
+
+	while getopts 'u:t:x' flag
+	do
+		case "${flag}" in
+			c) VCPUS="${OPTARG}";;
+			m) GB_RAM="${OPTARG}";;
+			d) STORAGE="${OPTARG}";;
+			s) COLIMA_SOCK="${OPTARG}";; 
+			h) COLIMA_HOST="${OPTARG}";; 
+		esac
+	done
+
 	colima stop
 	set-proxy
 	set-docker-host $COLIMA_HOST $COLIMA_SOCK
-	colima start
+	colima start --cpu $VCPUS --memory $GB_RAM --disk $STORAGE
 }
